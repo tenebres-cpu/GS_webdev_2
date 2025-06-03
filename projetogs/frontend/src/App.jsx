5018878307cd90997b17921ad95c2d5704317e53
Marcelo Affonso Fonseca <marcelo.anonimo2006@gmail.com>
2025-06-03 00:16:52 -0300

    modificando o App.jsx

--- projetogs/frontend/src/App.jsx
+++ projetogs/frontend/src/App.jsx
@@ -1,11 +1,26 @@
-
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './routes/Home'
+import Sobre from './routes/Sobre'
+import Solucao from './routes/Solucao'
+import Login from './routes/Login'
+import Cadastrar from './routes/Cadastrar'
+import Error from './routes/Error'
+ 
 function App() {
-
-
+  
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sobre" element={<Sobre />} />
+        <Route path="/solucao" element={<Solucao />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cadastrar" element={<Cadastrar />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
-
-export default App
+ 
+export default App;
